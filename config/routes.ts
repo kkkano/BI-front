bb96eda212004cf53f1e66958c61a02c6549b015
wx <657394554@qq.com>
2023-11-04 01:06:18 +0800

    1

---
+++ config/routes.ts
@@ -0,0 +1,20 @@
+﻿﻿export default [
+  { path: '/user', layout: false, routes: [{ path: '/user/login', component: './User/Login' }] },
+  { path: '/user', layout: false, routes: [{ path: '/user/Register', component: './User/Register' }] },//注册路由
+  { path: '/', redirect: '/add_chart' },
+  { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
+  { path: '/add_chart_async', name: '智能分析（异步）', icon: 'barChart', component: './AddChartAsync' },
+
+  { name:'我的图表',path: '/my_chart', icon: 'pieChart', component: './MyChart' },
+  {
+    path: '/admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    routes: [
+      { path: '/admin', name: '管理页面', redirect: '/admin/sub-page' },
+      { path: '/admin/sub-page', name: '管理页面2', component: './Admin' },
+    ],
+  },
+  { path: '/', redirect: '/welcome' },
+  { path: '*', layout: false, component: './404' },
+];
