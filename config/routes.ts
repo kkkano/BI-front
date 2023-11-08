ad22974e7321b70b49a00164a8031590cbe311eb
wx <657394554@qq.com>
2023-11-08 21:28:17 +0800

    基本功能已完成 待优化

--- config/routes.ts
+++ config/routes.ts
@@ -3,8 +3,8 @@
   { path: '/user', layout: false, routes: [{ path: '/user/Register', component: './User/Register' }] },//注册路由
   { path: '/', redirect: '/add_chart' },
   { path: '/add_chart', name: '智能分析', icon: 'barChart', component: './AddChart' },
-  { path: '/add_chart_async', name: '智能分析（异步）', icon: 'barChart', component: './AddChartAsync' },
-
+  { path: '/add_chart_async', name: '智能分析(异步 线程池实现)', icon: 'barChart', component: './AddChartAsync' },
+  { path: '/add_chart_async_mq', name: '智能分析(异步 MQ实现)', icon: 'barChart', component: './AddChartAsyncmq' },
   { name:'我的图表',path: '/my_chart', icon: 'pieChart', component: './MyChart' },
   {
     path: '/admin',
